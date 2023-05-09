842ced77421002dcc00ede41498d76734d65508b
hongkee01 <simbayippy@gmail.com>
2023-05-09 19:15:46 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `yipsinhang`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
